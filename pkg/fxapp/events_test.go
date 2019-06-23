590680dfe21ea57de0663771702169c20e4692e4
Alfio Zappala <oysterpack2016@gmail.com>
2019-06-23 18:50:11 -0400

    adding in support to log domain events, i.e., type safe log events

---
+++ pkg/fxapp/events_test.go
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2019 OysterPack, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fxapp_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/oysterpack/partire-k8s/pkg/fxapp"
+	"github.com/oysterpack/partire-k8s/pkg/ulidgen"
+	"github.com/rs/zerolog"
+	"go.uber.org/fx"
+	"testing"
+)
+
+type HealthCheckFailure struct {
+	healthCheckID string
+	err           error
+}
+
+func (event *HealthCheckFailure) MarshalZerologObject(e *zerolog.Event) {
+	e.Str("id", event.healthCheckID).
+		Err(event.err)
+}
+
+type LogHealthCheckFailure func(event HealthCheckFailure, tags ...string)
+
+func NewHealthCheckFailure(logger *zerolog.Logger) LogHealthCheckFailure {
+	logEvent := fxapp.NewLogEventFunc(logger, zerolog.ErrorLevel, "01DE2Z4E07E4T0GJJXCG8NN6A0")
+	return func(event HealthCheckFailure, tags ...string) {
+		logEvent(&event, "healthcheck failed", tags...)
+	}
+}
+
+func TestDomainEvent(t *testing.T) {
+	buf := new(bytes.Buffer)
+	app, err := fxapp.NewAppBuilder(newDesc("foo", "0.1.0")).
+		LogWriter(buf).
+		Provide(NewHealthCheckFailure).
+		Invoke(func(lc fx.Lifecycle, logHealthCheckFailure LogHealthCheckFailure) {
+			lc.Append(fx.Hook{
+				OnStart: func(_ context.Context) error {
+					healthCheckFailure := HealthCheckFailure{
+						ulidgen.MustNew().String(),
+						errors.New("failure to connect"),
+					}
+					logHealthCheckFailure(healthCheckFailure, "tag-a", "tag-b")
+					return nil
+				},
+			})
+		}).
+		Build()
+
+	switch {
+	case err != nil:
+		t.Errorf("*** app build failure: %v", err)
+	default:
+		go app.Run()
+		<-app.Started()
+		app.Shutdown()
+		<-app.Done()
+
+		t.Logf("\n%s", buf)
+	}
+
+}
--- pkg/fxapp/zerolog.go
+++ pkg/fxapp/zerolog.go
@@ -45,7 +45,9 @@ func init() {
 	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 }
 
-// EventLogger returns a new logger with the event name field 'n' set to the specified value.
+// EventLogger returns a new logger with the event type ID field 'n' set to the specified value.
+//
+// The event type ID should be unique. To ensure uniqueness, use ULIDs.
 func EventLogger(logger *zerolog.Logger, id string) *zerolog.Logger {
 	l := logger.With().Str("n", id).Logger()
 	return &l
@@ -87,4 +89,51 @@ func (level LogLevel) ZerologLevel() zerolog.Level {
 // Use Case: Enables log event to be referenced.
 func SetEventID(e *zerolog.Event, _ zerolog.Level, _ string) {
 	e.Str("z", ulidgen.MustNew().String())
+}
+
+// LogEvent logs events using a standardized structure, e.g.,
+//
+//
+func LogEvent(logger *zerolog.Logger, level zerolog.Level, eventTypeID string, eventObject zerolog.LogObjectMarshaler, msg string, tags ...string) {
+	event := logger.WithLevel(level)
+
+	if eventObject != nil {
+		data := zerolog.Dict()
+		eventObject.MarshalZerologObject(data)
+		event.Dict(eventTypeID, data)
+	}
+
+	if len(tags) > 0 {
+		event.Strs("g", tags)
+	}
+
+	event.Msg(msg)
+}
+
+/* NewLogEventFunc creates a new function used to log events using a standardized structure, e.g., app event
+
+	{
+	  "l": "error", -------------------------------------- event level
+	  "a": "01DE379HHM9Y3QYBDB4MSY7YYQ",
+	  "r": "01DE379HHNRJ4YS4NY4CMJX5YE",
+	  "x": "01DE379HHN2RRX9YQCG2DN9CHG",
+	  "n": "01DE2Z4E07E4T0GJJXCG8NN6A0", ----------------- event type ID
+	  "01DE2Z4E07E4T0GJJXCG8NN6A0": { -------------------- event type ID is used as event object dictionary key (optional)
+		"id": "01DE379HHNVHQE5G6NHN2BBKAT", -------------- event object data (optional)
+		"e": "failure to connect" ------------------------ event object data (optional)
+	  }, ------------------------------------------------- event object data (optional)
+	  "g": ["tag-a","tag-b"], ---------------------------- event tags (optional)
+	  "z": "01DE379HHNM87XT4PBHXYYBTYS",
+	  "t": 1561328928,
+	  "m": "healthcheck failed" -------------------------- event short description
+	}
+
+the event object data is logged as an event dictionary, using the event type ID as the key
+*/
+func NewLogEventFunc(logger *zerolog.Logger, level zerolog.Level, eventTypeID string) func(eventObject zerolog.LogObjectMarshaler, msg string, tags ...string) {
+	eventLogger := EventLogger(logger, eventTypeID)
+	return func(eventObject zerolog.LogObjectMarshaler, msg string, tags ...string) {
+		LogEvent(eventLogger, level, eventTypeID, eventObject, msg, tags...)
+	}
+
 }
