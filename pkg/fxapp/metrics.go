c5d676fece9076f7e62ab837ad10a5d3babc00eb
Alfio Zappala <oysterpack2016@gmail.com>
2019-06-26 10:12:47 -0400

    integrating prometheus metrics into fxapp
    - prometheus.Registerer and prometheus.Gatherer are provided by the fxapp.App
    - all metrics are labeled with app info: App ID, ReleaseID, and InstanceID

--- pkg/fxapp/metrics.go
+++ pkg/fxapp/metrics.go
@@ -15,3 +15,102 @@
  */
 
 package fxapp
+
+import (
+	dto "github.com/prometheus/client_model/go"
+)
+
+// FindMetricFamily returns the first metric family that matches the filter
+func FindMetricFamily(mfs []*dto.MetricFamily, accept func(mf *dto.MetricFamily) bool) *dto.MetricFamily {
+	for _, mf := range mfs {
+		if accept(mf) {
+			return mf
+		}
+	}
+	return nil
+}
+
+// FindMetricFamilies returns first metric families that match the filter
+func FindMetricFamilies(mfs []*dto.MetricFamily, accept func(mf *dto.MetricFamily) bool) []*dto.MetricFamily {
+	var result []*dto.MetricFamily
+	for _, mf := range mfs {
+		if accept(mf) {
+			result = append(result, mf)
+		}
+	}
+	return result
+}
+
+// MetricDesc is used to describe the metric
+type MetricDesc struct {
+	Name string
+	Help string
+	MetricType
+	Labels []string
+}
+
+// DescsFromMetricFamilies extracts metric descriptors from gathered metrics
+func DescsFromMetricFamilies(mfs []*dto.MetricFamily) []*MetricDesc {
+	if len(mfs) == 0 {
+		return nil
+	}
+
+	metrics := make([]*MetricDesc, len(mfs))
+	for i, mf := range mfs {
+		metrics[i] = NewMetricDesc(mf)
+	}
+
+	return metrics
+}
+
+// NewMetricDesc extracts the metric descriptor from the gathered metrics
+func NewMetricDesc(mf *dto.MetricFamily) *MetricDesc {
+	m := &MetricDesc{
+		Name:       *mf.Name,
+		Help:       *mf.Help,
+		MetricType: mapMetricType(*mf.Type),
+	}
+	if len(mf.Metric) > 0 {
+		m.Labels = getLabels(mf.Metric[0])
+	}
+	return m
+}
+
+// MetricType represents a metric type enum
+type MetricType uint8
+
+// metric type enum values
+const (
+	Untyped MetricType = iota
+	Counter
+	Gauge
+	Histogram
+	Summary
+)
+
+func mapMetricType(t dto.MetricType) MetricType {
+	switch t {
+	case dto.MetricType_COUNTER:
+		return Counter
+	case dto.MetricType_GAUGE:
+		return Gauge
+	case dto.MetricType_HISTOGRAM:
+		return Histogram
+	case dto.MetricType_SUMMARY:
+		return Summary
+	default:
+		return Untyped
+	}
+}
+
+func getLabels(m *dto.Metric) []string {
+	if len(m.Label) == 0 {
+		return nil
+	}
+
+	names := make([]string, len(m.Label))
+	for i, labelPair := range m.Label {
+		names[i] = *labelPair.Name
+	}
+	return names
+}
--- pkg/fxapp/metrics_test.go
+++ pkg/fxapp/metrics_test.go
@@ -19,14 +19,15 @@ package fxapp_test
 import (
 	"github.com/oysterpack/partire-k8s/pkg/fxapp"
 	"github.com/prometheus/client_golang/prometheus"
+	io_prometheus_client "github.com/prometheus/client_model/go"
 	"github.com/rs/zerolog"
+	"strings"
 	"testing"
 )
 
 // the app provides support for prometheus metrics automatically
 // - prometheus Gathererer and Registerer are automatically provided by the app
 func TestMetricsRegistryProvided(t *testing.T) {
-
 	type FooCounter prometheus.Counter
 
 	var metricRegisterer prometheus.Registerer
@@ -75,6 +76,322 @@ func TestMetricsRegistryProvided(t *testing.T) {
 		if metricGatherer == nil {
 			t.Error("*** prometheus.Gatherer target was not populated")
 		}
+	}
+}
+
+func TestMetricsContainAppLabels(t *testing.T) {
+	type FooCounter prometheus.Counter
+	var metricsGatherer prometheus.Gatherer
+	var appInstanceID fxapp.InstanceID
+	var appDesc fxapp.Desc
+	_, err := fxapp.NewBuilder(newDesc("foo", "0.1.0")).
+		Invoke(
+			// Given a custom metric is registered
+			func(metricRegisterer prometheus.Registerer) (FooCounter, error) {
+				counter := prometheus.NewCounter(prometheus.CounterOpts{
+					Name: "foo",
+					Help: "foo counter",
+				})
+				err := metricRegisterer.Register(counter)
+				if err != nil {
+					return nil, err
+				}
+
+				return counter, nil
+			},
+		).
+		Populate(&metricsGatherer, &appInstanceID, &appDesc).
+		Build()
+
+	switch {
+	case err != nil:
+		t.Errorf("*** abb build error: %v", err)
+	default:
+		mfs, err := metricsGatherer.Gather()
+		switch {
+		case err != nil:
+			t.Errorf("*** failed to gather metrics")
+		default:
+			// Then the custom metric is returned when gathering metrics
+			mf := fxapp.FindMetricFamily(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+				return *mf.Name == "foo"
+			})
+			if mf == nil {
+				t.Error("*** foo metric is not registered")
+			}
+
+			// And all metrics have app labels
+			for _, mf := range mfs {
+				labels := mf.GetMetric()[0].GetLabel()
+				hasLabel := func(name, value string) bool {
+					for _, l := range labels {
+						if l.GetName() == name && l.GetValue() == value {
+							return true
+						}
+					}
+					return false
+				}
+				// And the metric has the app labels
+				if !hasLabel(fxapp.AppIDLabel, appDesc.ID().String()) {
+					t.Errorf("*** app ID label is missing: %s", mf)
+				}
+				if !hasLabel(fxapp.AppReleaseIDLabel, appDesc.ReleaseID().String()) {
+					t.Errorf("*** app ReleaseID label is missing: %s", mf)
+				}
+				if !hasLabel(fxapp.AppInstanceIDLabel, appInstanceID.String()) {
+					t.Errorf("*** app instance ID label is missing: %s", mf)
+				}
+			}
+		}
+	}
+}
+
+func TestMetricGoCollectorRegistered(t *testing.T) {
+	var metricsGatherer prometheus.Gatherer
+	_, err := fxapp.NewBuilder(newDesc("foo", "0.1.0")).
+		Invoke(func() {}).
+		Populate(&metricsGatherer).
+		Build()
+
+	switch {
+	case err != nil:
+		t.Errorf("*** abb build error: %v", err)
+	default:
+		mfs, err := metricsGatherer.Gather()
+		switch {
+		case err != nil:
+			t.Errorf("*** failed to gather metrics")
+		default:
+			mfs = fxapp.FindMetricFamilies(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+				return strings.HasPrefix(mf.GetName(), "go_")
+			})
+			if len(mfs) == 0 {
+				t.Error("*** go collector metrics are not registered")
+			}
+		}
+	}
+}
+
+func TestMetricProcessCollectorRegistered(t *testing.T) {
+	var metricsGatherer prometheus.Gatherer
+	_, err := fxapp.NewBuilder(newDesc("foo", "0.1.0")).
+		Invoke(func() {}).
+		Populate(&metricsGatherer).
+		Build()
+
+	switch {
+	case err != nil:
+		t.Errorf("*** abb build error: %v", err)
+	default:
+		mfs, err := metricsGatherer.Gather()
+		switch {
+		case err != nil:
+			t.Errorf("*** failed to gather metrics")
+		default:
+			mfs = fxapp.FindMetricFamilies(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+				return strings.HasPrefix(mf.GetName(), "process_")
+			})
+			if len(mfs) == 0 {
+				t.Error("*** process metrics are not registered")
+			}
+		}
+	}
+}
+
+func TestFindMetricFamily(t *testing.T) {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "foo1",
+		Help: "foo 1 help",
+	}))
+	registry.MustRegister(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "foo2",
+		Help: "foo 2 help",
+	}))
+
+	mfs, e := registry.Gather()
+	if e != nil {
+		t.Fatalf("*** failed to gather metrics: %v", e)
+	}
+	mf := fxapp.FindMetricFamily(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+		return *mf.Name == "foo1"
+	})
+	if mf == nil {
+		t.Fatal("*** metric was not found")
+	}
+
+	mf = fxapp.FindMetricFamily(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+		return false
+	})
+	if mf != nil {
+		t.Fatal("*** metric should not have been found")
+	}
+}
+
+func TestFindMetricFamilies(t *testing.T) {
+	registry := prometheus.NewRegistry()
+	registry.MustRegister(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "foo",
+		Help: "foo help",
+		ConstLabels: prometheus.Labels{
+			"bar": "1",
+		},
+	}))
+	registry.MustRegister(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "foo",
+		Help: "foo help",
+		ConstLabels: prometheus.Labels{
+			"bar": "2",
+		},
+	}))
+	registry.MustRegister(prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "bar",
+		Help: "foo help",
+		ConstLabels: prometheus.Labels{
+			"bar": "2",
+		},
+	}))
+
+	mfs, e := registry.Gather()
+	if e != nil {
+		t.Fatalf("*** failed to gather metrics: %v", e)
+	}
+
+	{
+		mfs = fxapp.FindMetricFamilies(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+			return *mf.Name == "foo"
+		})
+		if len(mfs) != 1 {
+			t.Fatalf("*** wrong number of metrics were returned: %v", mfs)
+		}
+	}
+
+	{
+		mfs = fxapp.FindMetricFamilies(mfs, func(mf *io_prometheus_client.MetricFamily) bool {
+			return false
+		})
+		if len(mfs) > 0 {
+			t.Fatalf("*** no metrics should have been returned: %v", mfs)
+		}
+	}
+}
+
+func TestDescsFromMetricFamilies(t *testing.T) {
+	registry := prometheus.NewRegistry()
+
+	counter := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "counter",
+		Help: "counter help",
+		ConstLabels: prometheus.Labels{
+			"foo": "bar",
+		},
+	})
+
+	counterNoLabels := prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "counterNoLabels",
+		Help: "counter help",
+	})
+
+	counterVec := prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "counterVec",
+			Help: "counterVec help",
+			ConstLabels: prometheus.Labels{
+				"foo": "bar",
+			},
+		},
+		[]string{"x", "y", "z"},
+	)
+	// metric vecs do not get reported until 1 metric is observed
+	counterVec.WithLabelValues("1", "2", "3").Inc()
+	counterVec.WithLabelValues("4", "5", "6").Inc()
+
+	gauge := prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "gauge",
+		Help: "gauge help",
+		ConstLabels: prometheus.Labels{
+			"foo": "bar",
+		},
+	})
+
+	gaugeVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "gaugeVec",
+			Help: "gaugeVec help",
+			ConstLabels: prometheus.Labels{
+				"foo": "bar",
+			},
+		},
+		[]string{"x", "y", "z"},
+	)
+	gaugeVec.WithLabelValues("1", "2", "3").Inc()
+
+	histogram := prometheus.NewHistogram(prometheus.HistogramOpts{
+		Name: "histogram",
+		Help: "histogram help",
+		ConstLabels: prometheus.Labels{
+			"foo": "bar",
+		},
+		Buckets: []float64{1, 2, 3},
+	})
+
+	histogramVec := prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: "histogramVec",
+			Help: "histogramVec help",
+			ConstLabels: prometheus.Labels{
+				"foo": "bar",
+			},
+			Buckets: []float64{1, 2, 3},
+		},
+		[]string{"x", "y", "z"},
+	)
+
+	summary := prometheus.NewSummary(prometheus.SummaryOpts{
+		Name: "summary",
+		Help: "summary help",
+		ConstLabels: prometheus.Labels{
+			"foo": "bar",
+		},
+	})
+
+	summaryVec := prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name: "summaryVec",
+			Help: "summaryVec help",
+			ConstLabels: prometheus.Labels{
+				"foo": "bar",
+			},
+		},
+		[]string{"x", "y", "z"},
+	)
+
+	registry.MustRegister(
+		counter,
+		counterNoLabels,
+		counterVec,
+
+		gauge,
+		gaugeVec,
+
+		histogram,
+		histogramVec,
+
+		summary,
+		summaryVec,
+	)
+	mfs, e := registry.Gather()
+	if e != nil {
+		t.Fatalf("*** failed to gather metrics: %v", e)
+	}
+
+	metrics := fxapp.DescsFromMetricFamilies(nil)
+	if len(metrics) > 0 {
+		t.Errorf("*** no metrics should have been returned")
+	}
+	metrics = fxapp.DescsFromMetricFamilies(mfs)
+	for _, m := range metrics {
+		t.Log(m)
 	}
 
 }
